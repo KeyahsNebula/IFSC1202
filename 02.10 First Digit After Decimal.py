ce03ddca33fee7d2b5a777d6da41b6fe699a3147
KeyahsNebula <zeaster@ualr.edu>
2022-09-21 16:59:03 +0000

    done

--- 02.10 First Digit After Decimal.py
+++ 02.10 First Digit After Decimal.py
@@ -1,3 +1,4 @@
 a = input('Enter Number: ')
 c = a / 10
 print(c)
+a = int(input("enter first number: "))a = int(input("enter first number: "))a = int(input("enter first number: "))a = int(input("enter first number: "))a = int(input("enter first number: "))a = int(input("enter first number: "))a = int(input("enter first number: "))a = int(input("enter first number: "))a = int(input("enter first n
--- Exam One.py
+++ Exam One.py
@@ -1,13 +1,18 @@
 print("Winners and Losers - Human is Even, Computer is Odd")
 for x in range(1,5):
     print("Round: {}".format(x))
-    x = int(input("Human Guess: "))
+    a = int("Enter your guess: ")
+    print("Human Guess: " + a)
     import random
     v = int(random.randint(1,5))
     print("Computers Guess: " + v)
-    sum = (x+v) // 2
+    sum = (a+v) % 2
     if sum > 0:
         print("Sum is Odd")
-        else:
-            print("Sum is Even")
-    
+    else:
+        print("Sum is Even")
+    print("Human Score: - Computer Score: ")
+if Human Score > Computer Score:
+    print("Human Wins")
+    else:
+        print("Computer Wins")
