737821fd82542c73787270a47a733a90a2b1f895
KeyahsNebula <zeaster@ualr.edu>
2022-10-16 21:20:46 +0000

    oct 2022

---
+++ 03.01 Minimum of Three Numbers.py
@@ -0,0 +1,13 @@
+a = int(input('Enter First Number: '))
+b = int(input('Enter Second Number: '))
+c = int(input('Enter Third Number: '))
+if a < b:
+    print(a)
+    else:
+        print(b)
+else:
+    if c < b:
+        print(c)
+        else:
+            print(b)
+            
